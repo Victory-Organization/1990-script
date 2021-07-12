a9ca386346267b3027ecacc4475b4de38a669fda
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-12 16:35:23 +0530

    Update index.sh
    Update 2009

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 _() {
-  YEAR="2008"
+  YEAR="2009"
   echo "Github Username: "
   read -r USERNAME
 
@@ -10,11 +10,24 @@ _() {
 
   cd "${YEAR}" || exit
   git init
-  echo "Year **${YEAR}** - Hello Everyone a Geek was Born Here. My Name is Aashik KJ a Geek since windows Vista time. I love Windows Vista My friends are Hari,Lubna,Pranow,Alex" \
+  echo "Year **${YEAR}** - Windows 7 is a major release of the Windows NT operating system developed by Microsoft. 
+  It was released to manufacturing on July 22, 2009, and became generally available on October 22, 2009.[9] It is the successor to Windows Vista, 
+  released nearly three years earlier. It remained an operating system for use on personal computers, including home and business desktops, laptops, 
+  tablet PCs and media center PCs, and itself was replaced in November 2012 by Windows 8, the name spanning more than three years of the product. 
+  Until April 9, 2013, Windows 7 RTM provided content such as security updates, software updates, PC driver updates and technical support, 
+  after which installation of Service Pack 1 is required for users to receive support and updates. Windows 7's server counterpart, Windows Server 2008 R2, 
+  was released at the same time. The last supported version of Windows based on this operating system was released on July 1, 2011, 
+  entitled Windows Embedded POSReady 7. 
+  On January 12, 2016, Microsoft ended support for Internet Explorer versions older than Internet Explorer 11 on Windows 7.[10][11][12] 
+  Extended support ended on January 14, 2020, over ten years after the release of Windows 7, after which the operating system ceased receiving further support or 
+  security updates to most users, and all PCs that blocks Windows Update on Windows 7 versions newer than KB4499164 released in May 2019 displays a full-screen upgrade 
+  warning notification with an information page link starting from January 15, 2020. A support program is currently available for enterprises, providing security 
+  updates for Windows 7 for up to four years since the official end of life.[13] However, Windows Embedded POSReady 7, the last Windows 7 variant, continues to 
+  receive security updates until October 2021. " \
     >README.md
   git add .
   GIT_AUTHOR_DATE="${YEAR}-01-01T18:00:00" \
-    GIT_COMMITTER_DATE="${YEAR}-01-01T18:00:00" \
+    GIT_COMMITTER_DATE="${YEAR}-07-30T18:00:00" \
     git commit -m "${YEAR}"
   git remote add origin "https://github.com/${USERNAME}/${YEAR}.git"
   git branch -M main
