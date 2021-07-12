cb239b73d24ffc0aff2e2cc410481fd90b9c784d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-12 16:54:30 +0530

    Update index.sh
    2012 Update 362

--- index.sh
+++ index.sh
@@ -12,9 +12,8 @@ _() {
   git init
   echo "Year **${YEAR}** - Nasa to encrypt data after latest breach
 
-Nasa is to encrypt all its mobile computers after the loss of a laptop containing personal information about more than 10,000 staff
-
-A number of Indian celebrities including AR Rehana, sister of AR Rahman, Naresh Iyer, Sayanora, Abdul Salam and others are in town for the show.  " \
+Nasa is to encrypt all its mobile computers after the loss of a laptop containing personal information about more than 10,000 staff.
+Reported By Tech Guru Aashik Jubilee School Vijinapura.....  " \
     >README.md
   git add .
   GIT_AUTHOR_DATE="${YEAR}-11-16T18:00:00" \
