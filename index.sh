5920da6d313e4b58bc3bfd968b49ce9a36dc6c59
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-12 16:46:21 +0530

    Update index.sh
    update 2011 2

--- index.sh
+++ index.sh
@@ -10,20 +10,38 @@ _() {
 
   cd "${YEAR}" || exit
   git init
-  echo "Year **${YEAR}** - Windows 7 is a major release of the Windows NT operating system developed by Microsoft. 
-  It was released to manufacturing on July 22, 2009, and became generally available on October 22, 2009.[9] It is the successor to Windows Vista, 
-  released nearly three years earlier. It remained an operating system for use on personal computers, including home and business desktops, laptops, 
-  tablet PCs and media center PCs, and itself was replaced in November 2012 by Windows 8, the name spanning more than three years of the product. 
-  Until April 9, 2013, Windows 7 RTM provided content such as security updates, software updates, PC driver updates and technical support, 
-  after which installation of Service Pack 1 is required for users to receive support and updates. Windows 7's server counterpart, Windows Server 2008 R2, 
-  was released at the same time. The last supported version of Windows based on this operating system was released on July 1, 2011, 
-  entitled Windows Embedded POSReady 7. 
-  On January 12, 2016, Microsoft ended support for Internet Explorer versions older than Internet Explorer 11 on Windows 7.[10][11][12] 
-  Extended support ended on January 14, 2020, over ten years after the release of Windows 7, after which the operating system ceased receiving further support or 
-  security updates to most users, and all PCs that blocks Windows Update on Windows 7 versions newer than KB4499164 released in May 2019 displays a full-screen upgrade 
-  warning notification with an information page link starting from January 15, 2020. A support program is currently available for enterprises, providing security 
-  updates for Windows 7 for up to four years since the official end of life.[13] However, Windows Embedded POSReady 7, the last Windows 7 variant, continues to 
-  receive security updates until October 2021. " \
+  echo "Year **${YEAR}** - The eight-year-old daughter of popular Indian playback singer, KS Chitra, drowned in an Emirates Hills swimming pool. Chitra came to the UAE in connection with the AR Rahman Musical Concert in Sharjah.
+  The girl was found dead in the swimming pool of a villa in the Emirates Hills, said sources familiar with the accident.
+  Nandana is believed to be born with Down syndrome.
+
+A number of Indian celebrities including AR Rehana, sister of AR Rahman, Naresh Iyer, Sayanora, Abdul Salam and others are in town for the show. 
+The playback singer was staying at the residence of Dileep Rahulan, leading Indian businessman. AR Rahman, the Oscar Winning Indian musician is not attending the show.
+Known as the nightingale of South Indian music industry, KS Chitra used to take daughter along with her for the music shows as she was a special needs child.
+“The girl went towards the swimming pool as the room door was found open; she accidentally fell into swimming pool and drowned. Even though she was rescued
+and rushed to the Jebel Ali Cedar hospital, the girl died,” said sources familiar with the incident.
+
+Sources told Emirates 24l7 that the child accidentally fell into the swimming pool as there was no fence and security monitoring. 
+“She (Chitra) was readying to go for the programme rehearsal at 9am. When she went to the toilet, the baby probably slipped out of the 
+villa without notice and accidentally fell into the swimming pool. Dubai Police team has inspected the accident site.”
+The child's body was repatriated to Chennai on Thursday night after embalming. The funeral was held in Chennai on Friday evening. 
+Show goes ahead
+
+The organisers told this website that the musical show will go ahead despite this tragic accident. Shafi, Creative Director, Media Factory, 
+organiser of the AR Rahman Music Concert at the Sharjah Cricket Stadium, revealed to Emirates 24|7 that the singers will express their sympathy 
+and condolences to the Chitra through a special prayer session during the show.
+
+”All the singers including Hariharan, Sayanora and AR Rahman sisters are sad about the accident. 
+The show will go ahead and we have decided to conduct a special prayer programme for the little one during the show. 
+Chitra, who was the lead female singer with Hariharan, will not sing as she is preparing to repatriate the body. 
+We have expressed our deep sympathy and condolences on the unfortunate death of Nandana,” Shafi said, adding that 
+“all singers and their families opted to live in Dunes Hotel Apartment, Karama, but Chitra and her family decided to 
+stay at a friend’s house in Emirates Hills because she thought it would be safe and could give more attention to her daughter.”
+
+Fans not happy
+
+Indian fans were saddened by the accident involving the only daughter of South Indian nightingale. A section of KS Chitra fans questioned the organiser’s 
+decision to go ahead with the musical evening following the sad incident. “Major shows definitely involve money and business, but there is a human element also. 
+How can other artists and singers perform and entertain the audience, when such a sad thing has happened to one of their colleagues,” said one music lover. " \
     >README.md
   git add .
   GIT_AUTHOR_DATE="${YEAR}-04-14T18:00:00" \
